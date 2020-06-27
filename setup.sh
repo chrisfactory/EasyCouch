3f7ddb7b87796fc90b2e685f7f2a062e5eeda74b
Chris <chris.ohl67@gmail.com>
2020-06-27 23:32:57 +0200

    stack

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
 
-sudo sh -c "echo -n '[username]:' >> ${NGINX_FILES_PATH}/htpasswd/${VIRTUAL_HOST}"
-sudo sh -c "openssl passwd -apr1 >> ${NGINX_FILES_PATH}/htpasswd/${VIRTUAL_HOST}"
+cd easycouch-web-server
+docker-compose -f "docker-compose.yml" down
+docker-compose -f "docker-compose.yml" up -d --build
+
+cd ../easycouch-stack 
+docker-compose -f "docker-compose.yml" down
+docker-compose -f "docker-compose.yml" up -d --build
+
+# sudo sh -c "echo -n '[username]:' >> ${NGINX_FILES_PATH}/htpasswd/${VIRTUAL_HOST}"
+# sudo sh -c "openssl passwd -apr1 >> ${NGINX_FILES_PATH}/htpasswd/${VIRTUAL_HOST}"
